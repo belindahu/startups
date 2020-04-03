b3f367de0a10a95730c67b9708800d22177a1b03
Belinda <belindahu@college.harvard.edu>
2020-04-03 16:49:25 -0700

    shiny app - V1!

--- startups_shiny/app.R
+++ startups_shiny/app.R
@@ -10,15 +10,25 @@
 library(shiny)
 
 # Define UI for application that displays the plot
-ui <- fluidPage(
-    
-    # Application title
-    titlePanel("Top Ten States with the Most Startups"),
-    
-    # plot output is named distPlot
-    plotOutput("toptenPlot")
-    
-)
+ui <- navbarPage(
+    "Startups in the US",
+    tabPanel("Graphs",
+             titlePanel("Top Ten States with the Most Startups"),
+             plotOutput("toptenPlot")),
+    tabPanel("Discussion",
+             titlePanel("Discussion"),
+             h3("Methodology"),
+             h3("Challenges"),
+             h3("Future Directions"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is where I talk about my project."),
+             h3("About Me"),
+             p("My name is Belinda and I study History of Science. 
+             You can reach me at belindahu@college.harvard.edu.")))
 
 # # Define server logic required to display png
 server <- function(input, output) {
